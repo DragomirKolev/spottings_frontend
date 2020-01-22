e69154d1424735a76762e7c4a85ebc4452453c08
DRAGOMIR KOLEV <dragomirkolevreal@gmail.com>
2020-01-22 14:56:05 +0100

    Code cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,35 +4,33 @@ import axios from 'axios';
 import { actions as spottingsActions } from './spottings';
 import { actions as filtersActions } from './filter';
 
+const callActions = ( actionTypes, data ) => actionTypes.length > 0
+    && actionTypes.forEach((actionType) =>
+        store.dispatch({type: actionType, data: data.response}));
+
 const API_ROOT = 'http://localhost:5001/api/v1';
 
-const call = (method, path, actionTypes = [], data = null, options = null) => {
+const callApi = (method, path, actionTypes = [], data = null, options = null) => {
   axios({
     method,
     url: API_ROOT + path,
     data,
     options,
   })
-  .then((response) => {
-    actionTypes.length > 0 && 
-      actionTypes.forEach((actionType) => 
-        store.dispatch({type: actionType, data: response.data.response}))
-  })
-  .catch((error) => {
-    console.log('error = ', error);
-  })
+  .then((response) => callActions(actionTypes, response.data))
+  .catch((error) => console.log('error = ', error));
 };
 
 export const API = {
   spottings: {
     get() {
-      return call('GET', '/spottings', [spottingsActions.SET_SPOTTINGS, filtersActions.ADD_FILTER]);
+      return callApi('GET', '/spottings', [spottingsActions.SET_SPOTTINGS, filtersActions.ADD_FILTER]);
     },
     delete() {
-      return call('DELETE', '/spottings', [spottingsActions.DEL_SPOTTINGS]);
+      return callApi('DELETE', '/spottings', [spottingsActions.DEL_SPOTTINGS]);
     },
     post(data) {
-      return call('POST', '/spottings', [spottingsActions.ADD_SPOTTING], data);
+      return callApi('POST', '/spottings', [spottingsActions.ADD_SPOTTING], data);
     }
   },
 };
